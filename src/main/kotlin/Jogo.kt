7f9c7bb725c786a28209b954d6f1534435212334
jvictornascimento <victojoao789@gmail.com>
2024-07-03 18:56:24 -0300

    modelando projeto

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,10 +1,19 @@
 package br.com.alura
 
 data class Jogo(val titulo:String, val capa:String) {
+
+     var descricao:String? = null
+
+
     override fun toString(): String {
         return "Meu Jogo:\n" +
                 "Titulo: $titulo\n"+
-                "capa=$capa"
+                "capa: $capa\n"+
+                "Descrição: $descricao"
     }
 
 }
+
+
+
+
