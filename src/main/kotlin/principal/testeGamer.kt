2d28ab5a6a04299b9a2943913625164f30c50fde
jvictornascimento <victojoao789@gmail.com>
2024-07-03 21:39:57 -0300

    aprimoramos a busca

---
+++ src/main/kotlin/principal/testeGamer.kt
@@ -0,0 +1,25 @@
+import model.Gamer
+
+fun main(){
+    val gamer1 = Gamer(
+        "Jacque"
+        ,"jacque@emial.com.br")
+    println(gamer1)
+
+    val gamer2 = Gamer(
+        "Jeni"
+        ,"jeni@email.com"
+        ,"19/19/1992"
+        ,"jeniblo")
+    println(gamer2)
+
+    gamer1.let {
+        it.dataNascimento = "18/09/2000"
+        it.usuario = "jacqueskywalker"
+    }
+    println(gamer1.idInterno)
+    gamer1.usuario = "jacque"
+    println(gamer1)
+
+
+}
